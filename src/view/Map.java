617b6d38329f6432179280e6af9f99625cce0dec
Abdelrahman Fathy <abdof1723@gmail.com>
2021-06-18 22:20:21 +0200

    Map(cities)

---
+++ src/view/Map.java
@@ -0,0 +1,41 @@
+package view;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+import javafx.scene.layout.Border;
+
+public class Map extends JPanel {
+	private JButton cairo=new JButton("Cairo");
+	private JButton sparta=new JButton("Sparta");
+	private JButton rome=new JButton("Rome");
+	
+	public Map() {
+		setLayout(null);
+		setPreferredSize(new Dimension(1300,800));
+		cairo.setBounds(600,50,100,100);
+		sparta.setBounds(300,650,100,100);
+		rome.setBounds(900,650,100,100);
+		add(rome);
+		add(sparta);
+		add(cairo);
+	}
+	public static void main(String[] args) {
+		JFrame x=new JFrame();
+		x.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Map m=new Map();
+		x.setLayout(null);
+		m.setBounds(50,100, 1300,800);
+		m.setBackground(Color.red);
+		x.add( m);
+		JButton b=new JButton("hh");
+		b.setBounds(1350,100,550,800);
+		x.add(b,BorderLayout.EAST);
+		x.setVisible(true);
+		x.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		x.revalidate();
+		x.repaint();
+		
+	}
+}
